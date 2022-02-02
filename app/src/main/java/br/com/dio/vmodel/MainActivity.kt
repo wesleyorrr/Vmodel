125a0709f79114cf94c93c12fd478c58d4a14a7e
wesle_p1v78x7 <wesleyorrr@gmail.com>
2022-02-02 11:07:34 -0300

    View Model e lifecyle

---
+++ app/src/main/java/br/com/dio/vmodel/MainActivity.kt
@@ -0,0 +1,56 @@
+package br.com.dio.vmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var txtContador: EditText
+    lateinit var btnDados: Button
+    lateinit var  btnMostrar:Button
+
+
+    var contador: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initDados()
+        initContador()
+        initClick()
+
+        validaContador()
+    }
+
+    private fun validaContador() {
+        if(contador>5){
+            contador =0
+        }
+    }
+
+    private fun initClick() {
+        btnDados.setOnClickListener {
+            contador++
+            validaContador()
+            initContador()
+        }
+
+        btnMostrar.setOnClickListener {
+            Toast.makeText(this, "Valor Contador: ${ contador.toString()}",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initContador() {
+
+        txtContador.setText(contador.toString())
+    }
+
+    private fun initDados() {
+        txtContador = findViewById(R.id.txtContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
+    }
+}
